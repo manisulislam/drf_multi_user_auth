2b1a6f227ff8a1bcd9994286518facdc054618d1
anisul islam <anisulislam980340@gmail.com>
2024-02-23 19:14:57 +0600

    set up completed

---
+++ user/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+# Create your models here.
+
+
+
+class User(AbstractUser):
+    is_freelancer=models.BooleanField(default=False)
+    is_client=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+class Freelancer(models.Model):
+    user=models.OneToOneField(User,related_name='freelancer',on_delete=models.CASCADE)
+    phone=models.CharField(max_length=20)
+    skills=models.CharField(max_length=200)
+    description=models.TextField()
+    portfolio=models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.user.username
+
+class Client(models.Model):
+    user=models.OneToOneField(User, related_name='employer', on_delete=models.CASCADE)
+    company_name=models.CharField(max_length=200)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.company_name
