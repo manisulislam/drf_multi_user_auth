3e498d6254873a83987184be336d49a6cb25877b
anisul islam <anisulislam980340@gmail.com>
2024-02-23 23:22:36 +0600

    api completed

--- user/api/urls.py
+++ user/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import FreelancerSignUpView,ClientSignUpView
+from .views import FreelancerSignUpView,ClientSignUpView,CustomObtainToken,ClientOnlyView,FreelanceOnlyView
 
 urlpatterns=[
     path('signup/freelancer/',FreelancerSignUpView.as_view(),name='signup_freelancer'),
-    path('signup/client/',ClientSignUpView.as_view(),name='signup_client')
+    path('signup/client/',ClientSignUpView.as_view(),name='signup_client'),
+    path('login/', CustomObtainToken.as_view(), name='login'),
+    path('client/dashboard/',ClientOnlyView.as_view(),name='client_dashboard'),
+    path('freelancer/dashboard/', FreelanceOnlyView.as_view(), name='freelancer_dashboard'),
+    
     
 ]
--- user/api/views.py
+++ user/api/views.py
@@ -2,6 +2,11 @@ from .serializers import UserSerializer,FreelancerSignUpSerializer,ClientSignUpS
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import status
+from rest_framework.views import APIView
+from .permissions import IsClientUser,IsFreelanceUser
+from rest_framework import permissions
 
 
 
@@ -30,3 +35,39 @@ class ClientSignUpView(generics.CreateAPIView):
             "token": Token.objects.get(user=user).key,
             "message": "Freelancer account created successfully",
         })
+
+
+class CustomObtainToken(ObtainAuthToken):
+    def post(self,request,*args, **kwargs):
+        serializer = self.serializer_class(data=request.data,context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token,created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token':token.key,
+            'user_id':user.pk,
+            'is_client':user.is_client,
+            'email':user.email,
+        }
+        )
+
+
+class LogoutView(APIView):
+    def post(self, request,format=None):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
+class ClientOnlyView(generics.RetrieveAPIView):
+    permission_classes=[permissions.IsAuthenticated&IsClientUser]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+class FreelanceOnlyView(generics.RetrieveAPIView):
+    permission_classes=[permissions.IsAuthenticated&IsFreelanceUser]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
