9ebda0af9586a0b880878cf1cb257f43ebab2979
anisul islam <anisulislam980340@gmail.com>
2024-02-23 20:59:22 +0600

    sign up completed

--- user/api/urls.py
+++ user/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from .views import FreelancerSignUpView,ClientSignUpView
 
 urlpatterns=[
+    path('signup/freelancer/',FreelancerSignUpView.as_view(),name='signup_freelancer'),
+    path('signup/client/',ClientSignUpView.as_view(),name='signup_client')
     
 ]
--- user/api/views.py
+++ user/api/views.py
@@ -1,4 +1,4 @@
-from .serializers import FreelancerSignUpSerializer,ClientSignUpSerializer
+from .serializers import UserSerializer,FreelancerSignUpSerializer,ClientSignUpSerializer
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
